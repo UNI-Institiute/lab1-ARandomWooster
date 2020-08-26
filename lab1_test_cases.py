1ad5283a883abaed631ec1f7313be9818ed75405
repl.it user <replituser@example.com>
2020-08-26 01:18:26 +0000

    hello

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -12,8 +12,13 @@ import unittest      # import the module that supports writing unit tests
 class TestsLab1(unittest.TestCase):
    def test_expressions(self):         # Defines one testing function.
       self.assertEqual(0 + 1, 1)
+      self.assertEqual(2 * 2, 4)
+      self.assertEqual(19 // 3, 6)
+      self.assertAlmostEqual(19 / 3, 6.3)
+      self.assertAlmostEqual(19.0 / 3.0, 6.33)
+      self.assertEqual(4 * 2 + 27 // 3 + 4, 21)
+      self.assertEqual(4 * (2 + 27) // 3 + 4, 42)
       # Add code here (like the line above) for the additional test cases.
-
 
 # Run the unit tests.
 if __name__ == '__main__':
